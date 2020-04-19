0b15c505e25c8c43b9c15479f8a9184a1cd15158
jardianoalmeida <61389908+jardianoalmeida@users.noreply.github.com>
2020-04-19 11:59:01 -0300

    Componente Rodapé

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './componets/template/header/header.component';
 
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from './componets/template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
